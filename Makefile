8a814142459c492d559c3fa43dc4e172a0907517
Sida Say <sida.say@khalibre.com>
2023-03-06 17:45:18 +0700

    update to 3.8.1 and improvement

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
-info:
-	@echo "build	build the privacyidea image"
-	@echo "push		push the image to the docker hub"
-
 LOCAL_DATA_VOLUME=/tmp/privacyidea-data
+
+help: ## Show this help message
+	@echo "Usage: make [target]"
+	@echo ""
+	@echo "Targets:"
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "  %-30s %s\n", $$1, $$2}' | sort
 
 build: ## Build image
 	docker build -t khalibre/privacyidea:dev .
@@ -25,9 +27,11 @@ pipepper:
 	@echo PI_PEPPER=$(shell cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1) > pipepper
 
 cleanup:
-	docker stop privacyidea-dev || true
-	docker rm privacyidea-dev || true
-	sudo rm -rf $(LOCAL_DATA_VOLUME)
+	@if docker ps -a | grep -q privacyidea-dev; then docker stop privacyidea-dev || true; fi
+	@if docker ps -a | grep -q privacyidea-dev; then docker rm privacyidea-dev || true; fi
+	@if [ -d $(LOCAL_DATA_VOLUME) ]; then sudo rm -rf $(LOCAL_DATA_VOLUME); fi
 
 test:
 	container-structure-test test --image khalibre/privacyidea:dev --config structure-tests.yaml
+
+.DEFAULT_GOAL := help
--- rootfs/usr/local/bin/privacyidea_entrypoint.sh
+++ rootfs/usr/local/bin/privacyidea_entrypoint.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
+# This script sets up and starts PrivacyIDEA within a Docker container.
+
+# Source the common functions used by PrivacyIDEA scripts.
 source /usr/local/bin/_privacyidea_common.sh
 
 function main {
+    # Print a message indicating how to SSH into the container.
     echo "[PrivacyIDEA] To SSH into this container, run: \"docker exec -it ${HOSTNAME} /bin/bash\"."
     echo ""
 
+    # Set the mount directory for configuration files.
     if [ -d /etc/privacyidea/mount ]
     then
         PI_MOUNT_DIR=/etc/privacyidea/mount
     else
         PI_MOUNT_DIR=/mnt/privacyidea
     fi
-
     export PI_MOUNT_DIR
 
+    # Execute any pre-configuration scripts.
     execute_scripts /usr/local/privacyidea/scripts/pre-configure
 
+    # Configure Nginx.
     . configure_nginx.sh
 
+    # Execute any pre-startup scripts.
     execute_scripts /usr/local/privacyidea/scripts/pre-startup
 
+    # Start PrivacyIDEA.
     start_privacyidea
 
+    # Execute any post-shutdown scripts.
     execute_scripts /usr/local/privacyidea/scripts/post-shutdown
 }
 
+# Define the function to start PrivacyIDEA.
 function start_privacyidea {
     . start_privacyidea.sh
 }
 
+# Call the main function.
 main
